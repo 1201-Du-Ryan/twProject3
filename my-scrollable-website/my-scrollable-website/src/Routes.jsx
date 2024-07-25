9a9ce65f3d6adf13ea9efa465e6a4a01ed6cad7e
ychowdhury2024 <ychowdhury@addressincome.com>
2024-07-24 22:11:59 -0700

    Isolated Routes.jsx

--- my-scrollable-website/my-scrollable-website/src/Routes.jsx
+++ my-scrollable-website/my-scrollable-website/src/Routes.jsx
@@ -1,23 +1,23 @@
-// src/Routes.jsx
+// src/AppRoutes.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
 import CreateServicePage from './pages/CreateServicePage';
 import ServicePage from './pages/ServicePage';
 
-
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/landing" element={<LandingPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/create-service" element={<CreateServicePage />} />
-      <Route path="/service" element={<ServicePage />} />
-
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/create-service" element={<CreateServicePage />} />
+        <Route path="/service" element={<ServicePage />} />
+      </Routes>
+    </Router>
   );
 };
 
