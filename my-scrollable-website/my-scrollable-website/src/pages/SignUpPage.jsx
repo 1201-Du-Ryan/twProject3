a0f08eaae9e72acb136745b6f3c4388da1035ec2
ychowdhury2024 <ychowdhury@addressincome.com>
2024-08-01 12:42:37 -0700

    Input Box and Submit button components are made

--- my-scrollable-website/my-scrollable-website/src/pages/SignUpPage.jsx
+++ my-scrollable-website/my-scrollable-website/src/pages/SignUpPage.jsx
@@ -1,8 +1,36 @@
-// pages/SignUpPage.jsx
+//path = my-scrollable-website/my-scrollable-website/src/pages/SignUpPage.jsx
 import React from 'react';
+import { Container, Typography, Box } from '@mui/material';
+import InputBox from '../components/InputBox';
+import CustomButton from '../components/custom_button';
 
 const SignUpPage = () => {
-  return <h1>Sign Up Page</h1>;
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 8,
+          padding: 3,
+          borderRadius: 2,
+          boxShadow: 3,
+          backgroundColor: '#f5f5f5',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <form style={{ width: '100%', marginTop: 3 }}>
+          <InputBox label="Username*" type="text" />
+          <InputBox label="Email*" type="email" />
+          <InputBox label="Password*" type="password" />
+          <CustomButton title="Sign Up" type="submit" />
+        </form>
+      </Box>
+    </Container>
+  );
 };
 
 export default SignUpPage;
