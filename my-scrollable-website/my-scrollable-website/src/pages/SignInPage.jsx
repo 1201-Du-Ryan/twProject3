40eb52d15ddf5fcc2f631cf14241b58495195baa
ychowdhury2024 <ychowdhury@addressincome.com>
2024-08-01 14:39:06 -0700

    Added validation functions for email and password for authentication

--- my-scrollable-website/my-scrollable-website/src/pages/SignInPage.jsx
+++ my-scrollable-website/my-scrollable-website/src/pages/SignInPage.jsx
@@ -1,37 +1,85 @@
 // src/pages/SignInPage.jsx
-import React from 'react';
-import { Container, Typography, Box, Link } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, Box, Link, FormHelperText } from '@mui/material';
 import InputBox from '../components/InputBox';
 import CustomButton from '../components/custom_button';
+import { validatePassword, validateEmail } from '../utils/validationUtils';
+
+
 
 const SignInPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    setEmailError(validateEmail(newEmail));
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    setPasswordError(validatePassword(newPassword));
+  };
+
   return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginTop: 8,
-          padding: 3,
-          borderRadius: 2,
-          boxShadow: 3,
-          backgroundColor: '#f5f5f5',
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Sign In
-        </Typography>
-        <form style={{ width: '100%', marginTop: 3 }}>
-          <InputBox label="Username/Email" type="text" />
-          <InputBox label="Password" type="password" />
-          <CustomButton title="Sign In" type="submit" />
-          <Link href="#" variant="body2">
-            Forgot Password? Click here
-          </Link>
-        </form>
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        minWidth: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: 3,
+            borderRadius: 2,
+            boxShadow: 3,
+            backgroundColor: '#f5f5f5',
+          }}
+        >
+          <Typography component="h1" variant="h5" color="black">
+            Sign In
+          </Typography>
+          <form style={{ width: '100%', marginTop: 3 }}>
+            <InputBox 
+              label="Email*" 
+              type="email" 
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {emailError && (
+              <FormHelperText error>{emailError}</FormHelperText>
+            )}
+            <InputBox 
+              label="Password*" 
+              type="password" 
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            {passwordError && (
+              <FormHelperText error>{passwordError}</FormHelperText>
+            )}
+            <CustomButton title="Sign In" type="submit" disabled={!!passwordError || !!emailError} />
+            <Box sx={{ mt: 2 }}>
+              <Link href="#" variant="body2">
+                Forgot Password? Click here
+              </Link>
+            </Box>
+          </form>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
--- my-scrollable-website/my-scrollable-website/src/pages/SignUpPage.jsx
+++ my-scrollable-website/my-scrollable-website/src/pages/SignUpPage.jsx
@@ -1,36 +1,81 @@
 //path = my-scrollable-website/my-scrollable-website/src/pages/SignUpPage.jsx
-import React from 'react';
-import { Container, Typography, Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, Box, FormHelperText } from '@mui/material';
 import InputBox from '../components/InputBox';
 import CustomButton from '../components/custom_button';
+import { validatePassword, validateEmail } from '../utils/validationUtils';
+
+
 
 const SignUpPage = () => {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    setPasswordError(validatePassword(newPassword));
+  };
+
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    setEmailError(validateEmail(newEmail));
+  };
+
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginTop: 8,
-          padding: 3,
-          borderRadius: 2,
-          boxShadow: 3,
-          backgroundColor: '#f5f5f5',
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Sign Up
-        </Typography>
-        <form style={{ width: '100%', marginTop: 3 }}>
-          <InputBox label="Username*" type="text" />
-          <InputBox label="Email*" type="email" />
-          <InputBox label="Password*" type="password" />
-          <CustomButton title="Sign Up" type="submit" />
-        </form>
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        minWidth: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: 3,
+            borderRadius: 2,
+            boxShadow: 3,
+            backgroundColor: '#f5f5f5',
+          }}
+        >
+          <Typography component="h1" variant="h5" color="black">
+            Register
+          </Typography>
+          <form style={{ width: '100%', marginTop: 3 }}>
+            <InputBox label="Full Name*" type="text" />
+            <InputBox 
+              label="Email*" 
+              type="email" 
+              value={email}
+              onChange={handleEmailChange}
+            />
+            {emailError && (
+              <FormHelperText error>{emailError}</FormHelperText>
+            )}
+            <InputBox 
+              label="Password*" 
+              type="password" 
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            {passwordError && (
+              <FormHelperText error>{passwordError}</FormHelperText>
+            )}
+            <CustomButton title="Sign Up" type="submit" disabled={!!passwordError || !!emailError} />
+          </form>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
