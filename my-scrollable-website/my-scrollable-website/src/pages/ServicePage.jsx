6d178ec05954ffc5534b921a258f79f330f906c5
KBTrotter <ktrotter@nevada.unr.edu>
2024-07-31 21:31:55 -0400

    added service page skeleton and image carousel

--- my-scrollable-website/my-scrollable-website/src/pages/ServicePage.jsx
+++ my-scrollable-website/my-scrollable-website/src/pages/ServicePage.jsx
@@ -1,13 +1,33 @@
 // src/pages/ServicePage.jsx
 import React from 'react';
-
+import Box from '@mui/material/Box';
+import { Button, Container, Divider, Grid, Typography,  } from '@mui/material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import '../styles/ServicePage.css'; 
+import Images from '../components/image_carousel'
 const ServicePage = () => {
   
   return (
-    <div>
-      <h1>Service Page</h1>
-      {/* Details for the service with ID {id} */}
-    </div>
+    <Container className='service-page'>
+        <div className='service-section'>
+          <Images/>
+              <section>
+                <h2>Service Title</h2>
+                <h1>$50.00</h1>
+                <Button className='buy-button' variant='contained' startIcon={<AddShoppingCartIcon/>}>Buy</Button>
+              </section>
+        </div>
+        <Divider/>
+        <Box>
+          <h4>About Service</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos reprehenderit aperiam possimus repellat, aut animi et veniam vero nihil. 
+            Ipsum, illo eos iste minima eligendi dicta fugit libero. Aperiam, eius. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Sed sint illum ea dolor quae. Excepturi non eligendi, fugiat animi iusto aliquid soluta est nulla quasi iste qui consectetur esse eveniet.<br/> 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, blanditiis nesciunt, fugiat deserunt voluptates, molestiae cum vero quis itaque quasi o
+            dit quod eius unde. Praesentium, laudantium! Rerum qui consequuntur vero.</p>
+        </Box>
+    </Container>
+
   );
 };
 
